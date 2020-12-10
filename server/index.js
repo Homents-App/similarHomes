b154c9864e59dc18ad3116c7d262d438f2555430
Zachary McCain <zacmmcca@gmail.com>
2020-12-10 16:39:06 -0600

    Updated server

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression())
 
+app.use(express.static(__dirname + '/../client/dist'));
 app.use('/:id', express.static(__dirname + '/../client/dist'));
 
 
